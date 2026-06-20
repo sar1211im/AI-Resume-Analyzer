83dc01fca229c0b84f054d520e16658414a7ea4d
Sheikh Sarim <152473025+sar1211im@users.noreply.github.com>
2026-06-20 07:48:41 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-main-gradient p-4 sm:p-6 lg:p-8 flex items-center justify-center">
+      <h1 className="text-7xl text-white">AI RESUME ANALYZER</h1>
+    </div>
+  );
+}
+
+export default App;
